b214c546daa6cc4e7cb8043fd61e55445dcc5376
MrXiaoM <coolxiaom95@gmail.com>
2023-10-31 19:39:47 +0800

    update docs

--- sidebar.js
+++ sidebar.js
@@ -3,7 +3,17 @@ const sidebar = {
         {
             text: "基础",
             children: [
-                "/server/basic/login"
+                "/server/basic/login",
+                "/server/basic/commands",
+                "/server/basic/skin"
+            ]
+        },
+        {
+            text: "游戏内容",
+            children: [
+                "/server/play/upgrade",
+                "/server/play/cuilian",
+                "/server/play/baptism"
             ]
         }
     ],
